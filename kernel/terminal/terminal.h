ca1a6cc3fef72b75f2d95f8a4cc8d8ae84d39d4b
Olen T <elroylilly@gmail.com>
2026-03-22 17:14:49 -0400

    Update terminal.h

--- kernel/terminal/terminal.h
+++ kernel/terminal/terminal.h
@@ -11,6 +11,9 @@ void printf(char* data, uint8_t COLOR);
 
 void vga_scroll(uint8_t color);
 
+// Ember2819: clear command
+void terminal_clear(uint8_t color);
+
 void input(unsigned char* buff, size_t buffer_size, uint8_t color);
 
 #endif
