722e5110a84f22337669b29aca1ccab12597e550
Pumpkicks <154643665+flixytss@users.noreply.github.com>
2026-03-27 14:13:02 -0400

    Add poweroff macros for QEMU, VirtualBox, and Cloud Hypervisor

--- kernel/bootoptions.h
+++ kernel/bootoptions.h
@@ -5,4 +5,14 @@
 
 #define reboot() outb(0x64, 0xfe);
 
+// QEMU
+#define QEMU_poweroff() outw(0x604, 0x2000); /* Only for debugging (QEMU only) */
+#define QEMU2_0_poweroff() outw(0xB004, 0x2000); /* Only for debugging (For olders version of QEMU (than 2.0)) */
+
+// VirtualBox poweroff
+#define Vb_poweroff() outw(0x4004, 0x3400);
+
+// Cloud Hypervisor poweroff
+#define Ch_poweroff() outw(0x600, 0x34);
+
 #endif
