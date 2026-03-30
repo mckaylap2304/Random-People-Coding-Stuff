349c05c347cac8681fce61964f28c4cdc6870185
Olen T <elroylilly@gmail.com>
2026-03-30 14:42:26 -0500

    Update commands.h

--- kernel/commands.h
+++ kernel/commands.h
@@ -1,7 +1,6 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-// We need uint8_t for the color parameter
 #include <stdint.h>
 
 // MorganPG1 - Moved some declarations from commands.c to commands.h
@@ -10,6 +9,7 @@ typedef struct {
     void (*func)(uint8_t color);
 } Command;
 
+// ---- Original commands ----
 static void cmd_help(uint8_t color);
 static void cmd_hello(uint8_t color);
 static void cmd_contributors(uint8_t color);
@@ -41,7 +41,31 @@ static void cmd_touch(uint8_t color);
 //ember2819
 static void cmd_gk_run_file(const char* filename, uint8_t color);
 
+// User / permission management
+static void cmd_whoami(uint8_t color);
+static void cmd_users(uint8_t color);
+static void cmd_useradd(uint8_t color);
+static void cmd_userdel(uint8_t color);
+static void cmd_passwd(uint8_t color);
+static void cmd_su(uint8_t color);
+static void cmd_logout(uint8_t color);
+
+// Filesystem utilities
+static void cmd_rm(uint8_t color);
+static void cmd_cp(uint8_t color);
+static void cmd_mv(uint8_t color);
+static void cmd_mkdir(uint8_t color);
+static void cmd_echo(uint8_t color);
+static void cmd_write(uint8_t color);
+
+// System info
+static void cmd_uptime(uint8_t color);
+static void cmd_meminfo(uint8_t color);
+
+// Dispatcher
 static int streq(unsigned char *a, char *b);
 void run_command(unsigned char *input, uint8_t color);
+
+void do_login_prompt(void);
 
 #endif
